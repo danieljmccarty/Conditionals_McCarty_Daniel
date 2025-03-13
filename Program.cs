87607decd37186fd2ce8067bfa3de792c8276ffd
danieljmccarty <danmccarty07@gmail.com>
2025-03-13 14:58:39 -0500

    Comments

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("enter a whole number, please!");
             int myWholeNum = Convert.ToInt32(Console.ReadLine());
 
+            // check if positive or negitive
             if (myWholeNum >= 0) 
             {
                 Console.WriteLine($"{myWholeNum} is a positive number!");
@@ -16,6 +18,7 @@
                 Console.WriteLine($"{myWholeNum} is a negative number!");
             }
 
+            // check if even or odd
             if(myWholeNum % 2 == 0)
             {
                 Console.WriteLine($"{myWholeNum} is a even number!");
@@ -25,16 +28,23 @@
                 Console.WriteLine($"{myWholeNum} is a odd number!");
             }
 
+            // creates two or more integers numbers
             int num1 = 6;
             int num2 = 99;
 
+            // gets first max
             int firstMax = Math.Max(myWholeNum, num1);
+            // get maximum number
             int max = Math.Max(firstMax, num2);
 
+            // prints max to console
             Console.WriteLine($"The maximum number of {myWholeNum}, {num1}, and {num2} is {max}!");
+
+            // asks to enter a grade and assigns it to a char variable, grade
             Console.WriteLine("Enter a grade (A, B, C, D, F)!");
             char grade = Console.ReadKey().KeyChar;
 
+            //prints description based on grade value
             switch (grade) 
             {
                 case 'A':
@@ -53,7 +63,7 @@
                     Console.WriteLine("\nFail");
                     break;
                 default:
-                    Console.WriteLine("\nSuperior");
+                    Console.WriteLine("\nyou did not enter a valid grade!");
                     break;
             }
         }
